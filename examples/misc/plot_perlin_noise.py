b0c7d03e27f14864e21fb9008c5fa10df4081519
Louis Tiao <louistiao@gmail.com>
2020-06-29 23:41:34 +0200

    updates

--- examples/misc/plot_perlin_noise.py
+++ examples/misc/plot_perlin_noise.py
@@ -13,22 +13,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from scipy.special import expit
 from noise import pnoise2
 
 h, w = 200, 200
 
-factor = 10.0
+octaves = 3
+factor = 3.0
 
 num_steps = 50
 step_size = 1.0
 
 max_size = step_size * num_steps
 
+seed = 42  # set random seed for reproducibility
+random_state = np.random.RandomState(seed)
+
 y, x = np.ogrid[:h, :w]
 X, Y = np.broadcast_arrays(x, y)
 
-Z = expit(factor * np.vectorize(pnoise2)(x/w, y/h, octaves=6))  # range [-1, 1]
+Z = np.tanh(factor * np.vectorize(pnoise2)(x/w, y/h, octaves=octaves))  # range [-1, 1]
 theta = np.pi * Z  # range [-pi, pi]
 
 # %%
@@ -96,43 +99,86 @@ plt.show()
 # Path
 # ----
 
-num_lines = 5000
 
-lines_x = []
-lines_y = []
+# TODO: `w`, `h` arguments
+def line(x, y, x_lim, y_lim, step_fn, num_steps, step_size=1.0):
 
-for l in range(num_lines):
+    x_min, x_max = x_lim
+    y_min, y_max = y_lim
 
-    x = w * np.random.rand()
-    y = h * np.random.rand()
+    if not (num_steps and x_min <= x < x_max and y_min <= y < y_max):
+        return [], []
 
-    xs = [x]
-    ys = [y]
+    dx, dy = step_fn(x, y, step_size=step_size)
 
-    for step in range(num_steps):
+    xs, ys = line(x=x+dx, y=y+dy, x_lim=x_lim, y_lim=y_lim, step_fn=step_fn,
+                  num_steps=num_steps-1, step_size=step_size)
+    xs.append(x)
+    ys.append(y)
 
-        j, i = int(x), int(y)
+    return xs, ys
 
-        if not (0 <= i < w and 0 <= j < h):
-            break
 
-        x += step_size * np.cos(theta[i, j])
-        y += step_size * np.sin(theta[i, j])
+# %%
+def step(x, y, step_size):
 
-        xs.append(x)
-        ys.append(y)
+    j, i = int(x), int(y)
 
-    lines_x.append(xs)
-    lines_y.append(ys)
+    dx = step_size * np.cos(theta[i, j])
+    dy = step_size * np.sin(theta[i, j])
+
+    return dx, dy
+
+
+# %%
+x = y = 25
+
+xs, ys = line(x, y, x_lim=(0, w), y_lim=(0, h), step_fn=step,
+              num_steps=num_steps, step_size=5.0)
+_xs = np.asarray(xs[::-1])
+_ys = np.asarray(ys[::-1])
+
+# %%
 
 fig, ax = plt.subplots(figsize=(10, 8))
 
-for xs, ys in zip(lines_x, lines_y):
-
-    # ax.plot(xs, ys, marker='o', linestyle="none", mfc="none")
-    ax.plot(xs, ys)
+ax.quiver(_xs[:-1], _ys[:-1], _xs[1:] - _xs[:-1], _ys[1:] - _ys[:-1],
+          scale_units='xy', angles='xy', scale=1.0, width=3e-3, color='r')
+ax.quiver(xdown, ydown, dx, dy)
 
 ax.set_xlabel(r"$x$")
 ax.set_ylabel(r"$y$")
+
+plt.show()
+# %%
+
+num_lines = 600
+step_sizes = 0.5 * (np.arange(10) + 1.0)
+
+dfs = []
+
+for step_size in step_sizes:
+
+    for lineno in range(num_lines):
+
+        x = w * random_state.rand()
+        y = h * random_state.rand()
+
+        xs, ys = line(x, y, x_lim=(0, w), y_lim=(0, h), step_fn=step,
+                      num_steps=num_steps, step_size=step_size)
+
+        df = pd.DataFrame(dict(lineno=lineno, stepsize=step_size, x=xs, y=ys))
+        dfs.append(df)
+# %%
+
+data = pd.concat(dfs, axis="index", sort=True)
+data
+# %%
+
+fig, ax = plt.subplots(figsize=(10, 8))
+
+sns.lineplot(x='x', y='y', hue='stepsize', units='lineno', estimator=None,
+             sort=False, palette='Spectral', legend=None, linewidth=1.0,
+             alpha=0.4, data=data, ax=ax)
 
 plt.show()
