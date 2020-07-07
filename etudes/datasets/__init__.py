fa4f55aea0054ddff4d57b83b1268f6e17b3172e
Louis Tiao <louistiao@gmail.com>
2020-07-07 16:55:23 +0200

    cleaned up and restructured

---
+++ etudes/datasets/__init__.py
@@ -0,0 +1,15 @@
+from .base import (mauna_loa_load_dataframe,
+                   coal_mining_disasters_load_data,
+                   load_bee_dance_dataframe,
+                   binary_mnist_load_data)
+from .synthetic import make_regression_dataset, make_classification_dataset
+
+
+__all__ = [
+    "make_regression_dataset",
+    "make_classification_dataset",
+    "mauna_loa_load_dataframe",
+    "coal_mining_disasters_load_data",
+    "load_bee_dance_dataframe",
+    "binary_mnist_load_data"
+]
--- examples/misc/plot_perlin_noise.py
+++ examples/misc/plot_perlin_noise.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Perlin noise
-============
+Vector Field Generative Art with Perlin noise
+=============================================
 
 Hello world
 """
@@ -15,28 +15,44 @@ import seaborn as sns
 
 from noise import pnoise2
 
-h, w = 200, 200
-
-octaves = 3
+octaves = 5
 factor = 3.0
 
+num_lines = 600
 num_steps = 50
-step_size = 1.0
-
-max_size = step_size * num_steps
+step_sizes = 0.5 * (np.arange(10) + 1.0)
 
 seed = 42  # set random seed for reproducibility
 random_state = np.random.RandomState(seed)
 
+# resolution
+h, w = 200, 200
+
 y, x = np.ogrid[:h, :w]
 X, Y = np.broadcast_arrays(x, y)
 
+# %%
+# The function ``pnoise2`` has range [-1, 1]. However, the outputs tend mostly
+# to be centered around 0. Let's blow up the range by some ``factor`` and
+# squash it through the :math:`\tanh` function so that it is still in the
+# desired range.
 Z = np.tanh(factor * np.vectorize(pnoise2)(x/w, y/h, octaves=octaves))  # range [-1, 1]
 theta = np.pi * Z  # range [-pi, pi]
 
 # %%
-# Foo
-# ---
+# Sparsify the grid so we can later draw intelligible quiver plots.
+w_factor = h_factor = 10
+
+x_sparse = x[..., ::w_factor]
+y_sparse = y[::h_factor]
+
+X_sparse = X[::h_factor, ::w_factor]
+Y_sparse = Y[::h_factor, ::w_factor]
+
+theta_sparse = theta[::w_factor, ::w_factor]
+
+# %%
+# We use use Perlin noise at every grid point as the angle (in radians).
 fig, ax = plt.subplots(figsize=(10, 8))
 
 ax.set_title(r"angle $\theta$ (rad)")
@@ -50,27 +66,13 @@ ax.set_ylabel(r"$y$")
 plt.show()
 
 # %%
-# Bar
-# ---
-
-hr, wr = 10, 10
-
-ydown = y[::hr]
-xdown = x[..., ::wr]
-theta_down = theta[::hr, ::wr]
-Xdown = X[::hr, ::wr]
-Ydown = Y[::hr, ::wr]
-
-data = pd.DataFrame(theta_down / np.pi,
-                    index=ydown.squeeze(axis=1),
-                    columns=xdown.squeeze(axis=0))
-
-dx = step_size * np.cos(theta_down)
-dy = step_size * np.sin(theta_down)
+# For sanity-check, we view the numerical values of the angles normalized by
+# :math:`\pi` on the heatmap.
+data = pd.DataFrame(theta_sparse / np.pi,
+                    index=y_sparse.squeeze(axis=1),
+                    columns=x_sparse.squeeze(axis=0))
 
 # %%
-# Baz
-# ---
 fig, ax = plt.subplots(figsize=(10, 8))
 
 ax.set_title(r"angle $\theta / \pi$ (rad)")
@@ -84,11 +86,16 @@ ax.set_ylabel(r"$y$")
 plt.show()
 
 # %%
-# Vector field
-# ------------
+# Flow field
+# ----------
+
+dx = np.cos(theta_sparse)
+dy = np.sin(theta_sparse)
+# %%
+
 fig, ax = plt.subplots(figsize=(10, 8))
 
-ax.quiver(xdown, ydown, dx, dy)
+ax.quiver(x_sparse, y_sparse, dx, dy)
 
 ax.set_xlabel(r"$x$")
 ax.set_ylabel(r"$y$")
@@ -144,7 +151,7 @@ fig, ax = plt.subplots(figsize=(10, 8))
 
 ax.quiver(_xs[:-1], _ys[:-1], _xs[1:] - _xs[:-1], _ys[1:] - _ys[:-1],
           scale_units='xy', angles='xy', scale=1.0, width=3e-3, color='r')
-ax.quiver(xdown, ydown, dx, dy)
+ax.quiver(x_sparse, y_sparse, dx, dy)
 
 ax.set_xlabel(r"$x$")
 ax.set_ylabel(r"$y$")
@@ -152,15 +159,13 @@ ax.set_ylabel(r"$y$")
 plt.show()
 # %%
 
-num_lines = 600
-step_sizes = 0.5 * (np.arange(10) + 1.0)
-
 dfs = []
 
 for step_size in step_sizes:
 
     for lineno in range(num_lines):
 
+        # sample starting point uniformly at random
         x = w * random_state.rand()
         y = h * random_state.rand()
 
@@ -180,5 +185,7 @@ fig, ax = plt.subplots(figsize=(10, 8))
 sns.lineplot(x='x', y='y', hue='stepsize', units='lineno', estimator=None,
              sort=False, palette='Spectral', legend=None, linewidth=1.0,
              alpha=0.4, data=data, ax=ax)
+ax.set_xlabel(r"$x$")
+ax.set_ylabel(r"$y$")
 
 plt.show()
